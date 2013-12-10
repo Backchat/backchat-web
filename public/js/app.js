519719f75e45c4bfa1100201d302a04d933faec9
James Anderson <me@jamesaanderson.com>
2013-12-09 20:10:37 -0800

    Fix video bug

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   $('#content').on('click', function() {
     $('#video-modal').css('display', 'block');
+    $('#video-modal').append("<iframe width='960' height='540' src='//www.youtube-nocookie.com/embed/y6QGP0OUaV4?rel=0&autoplay=1' frameborder='0' allowfullscreen></iframe>");
     $('#overlay').css('display', 'block');
   });
 
   $('#overlay').on('click', function() {
     $('#video-modal').css('display', 'none');
-    $('#video-modal iframe').attr('src', $('#video-modal iframe').attr('src'));
+    $('#video-modal iframe').remove();
     $('#overlay').css('display', 'none');
   });
 });
