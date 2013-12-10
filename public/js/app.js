8413460c34039cd008f5832c835b94ea27eedaf8
James Anderson <me@jamesaanderson.com>
2013-12-09 19:46:55 -0800

    Modal edits

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   $('#content').on('click', function() {
-    $('#modal').modal();
+    $('#video-modal').css('display', 'block');
+    $('#overlay').css('display', 'block');
+  });
+
+  $('#overlay').on('click', function() {
+    $('#video-modal').css('display', 'none');
+    $('#video-modal iframe').attr('src', $('#video-modal iframe').attr('src'));
+    $('#overlay').css('display', 'none');
   });
 });
