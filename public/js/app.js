5ab6aa70d89f09110c2a1cb0732ac14584fdb972
Ben <benbd86@gmail.com>
2014-01-08 15:57:14 -0800

    General fixes
    Two new shared files - footer, download included in the rest.
    Added link to android version on PlayStore

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  var isVisible=0;
   $('#content').on('click', function() {
     $('#video-modal').css('display', 'block');
     $('#video-modal').append("<iframe width='960' height='540' src='//www.youtube-nocookie.com/embed/y6QGP0OUaV4?rel=0&autoplay=1' frameborder='0' allowfullscreen></iframe>");
@@ -11,13 +12,19 @@ $(document).ready(function() {
     $('#video-overlay').css('display', 'none');
   });
 
-  $('#download').on('click', function() {
-    $('#download-modal').css('display', 'block');
-    $('#download-overlay').css('display', 'block');
-  });
-
-  $('#download-overlay').on('click', function() {
-    $('#download-modal').css('display', 'none');
-    $('#download-overlay').css('display', 'none');
+  $('#inc-footer').on('click', function() {
+    switch (isVisible)
+    {
+    case 0:
+      $('#download-modal').css('display', 'block');
+      $('#download-overlay').css('display', 'block');
+      isVisible=1;
+      break;
+    case 1:
+      $('#download-modal').css('display', 'none');
+      $('#download-overlay').css('display', 'none');
+      isVisible=0;
+      break;
+    }
   });
 });
